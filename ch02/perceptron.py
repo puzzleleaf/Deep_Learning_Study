f9068da6aff75fee784655d77b9d6f6758ad3861
PuzzleLeaf <cmtyx2@naver.com>
2017-12-03 17:57:05 +0900

    퍼셉트론 끝

--- ch02/perceptron.py
+++ ch02/perceptron.py
@@ -1,12 +1,47 @@
-def AND(x1, x2):
-	w1, w2, theta = 0.5, 0.5, 0.7
-	tmp = x1*w1 + x2*w2
-	if tmp <= theta:
+import numpy as np
+
+def AND(x1, x2) :
+	x = np.array([x1, x2]) # 입력 값
+	w = np.array([0.5, 0.5]) # 가중치
+	b = -0.7 # 편향 
+	tmp = np.sum(w*x) + b
+	if tmp <= 0 :
 		return 0
-	elif tmp > theta :
+	else :
 		return 1
 
-print(AND(0,0))
-print(AND(1,0))
-print(AND(1,1))
-print(AND(0,1))
+#NAND는 AND의 반대
+def NAND(x1, x2) :
+	x = np.array([x1, x2])
+	w = np.array([-0.5, -0.5])
+	b = 0.7
+	tmp = np.sum(w*x) + b
+	if tmp <= 0 :
+		return 0
+	else :
+		return 1 
+
+def OR(x1 ,x2) :
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.2
+	tmp = np.sum(w*x) + b
+	if tmp <= 0 :
+		return 0
+	else :
+		return 1 
+
+# AND, NAND, OR은 모두 같은 구조의 퍼셉트론이고 서로 가중치의 매개변수의 값만 다르다
+
+# XOR - NAND와 OR, AND의 조합으로 만들 수 있다.
+
+def XOR(x1, x2) :
+	s1 = NAND(x1, x2)
+	s2 = OR(x1, x2)
+	y = AND(s1, s2)
+	return y
+
+print(XOR(0,0))
+print(XOR(0,1))
+print(XOR(1,0))
+print(XOR(1,1))
