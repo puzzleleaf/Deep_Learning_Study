1725b942da9409611e0384c882e89da8d318da87
PuzzleLeaf <cmtyx2@naver.com>
2018-04-15 18:38:22 +0900

    배치 처리

--- ch03/sigmoid.py
+++ ch03/sigmoid.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sys, os
+import pickle
 sys.path.append(os.pardir)
 from dataset.minist import load_mnist
 from PIL import Image
@@ -28,14 +29,37 @@ def img_show(img) :
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten = True, normalize = False)
+def get_data() :
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten = True, normalize = True)
+    return x_test, t_test
 
-img = x_train[0]
-label = t_train[0]
-print(label)
+def init_network() :
+    with open("sample_weight.pkl", 'rb') as f :
+        network = pickle.load(f)
+    return network
 
-print(img.shape)
-img = img.reshape(28, 28)
-print(img.shape)
+def predict(network, x) :
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-img_show(img)
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+    return y
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100 # 배치 크기
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size) :
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1) # 1번째 차원에서 최댓값의 인덱스
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+print("Accuracy :" + str(float(accuracy_cnt) / len(x)))
